2f7351f36a756ca31638ff556ddb2ef8e880a623
Veronica Williams <vcwilliams2014@gmail.com>
2017-11-27 23:19:37 -0800

    Updating Files

--- project.js
+++ project.js
@@ -36,13 +36,36 @@ choices:["Comedy", "Drama", "SciFi", "Thrillers", "Horror"],
 name:"movie"
 },
 ])
-.then(function(res.confirm){
-if (res.confirm){console.log("\nIt's great to know that you are excited about coding.")
+.then(function(res){
+if (res.confirm){console.log("\nIt's great to know that you are excited about coding" + res.firstname + ".");
+}
+else{
 
+	console.log("\nThis homework assignemnt doesn't give me time to review my coding challenges. I need to go back to the beginning of javascript again.\n");
+}
+});
 
+// below...I will create a constructor for a user.
+
+function User(name, hobby, movie, confirm){
+this.name = name;
+this.hobby = hobby;
+this.movie = movie;
+this.confirm = confirm;
+this.res = function(){
+
+	if(this.confirm === true){
+
+		console.log("You are freakin' awesome!");
+	}
+};
 
 }
 
+var userOne = new User("Veronica", "Working Out", "The Sandlot", true);
+var userTwo = new User("Martha", "Coding", "Baby Driver", true);
+
+userOne.res();
+userTwo.res();
 
 
-})
